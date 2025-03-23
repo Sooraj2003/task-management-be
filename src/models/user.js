2a775ee53d0383dac1ffba1bf540ad0e5fd3a4a1
Sooraj2003 <96384011+Sooraj2003@users.noreply.github.com>
2025-03-23 16:32:14 +0530

    created userSchema and wrote userSchema methods

---
+++ src/models/user.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+
+const userSchema =  mongoose.Schema({
+   firstName:{
+    type:String,
+    required:true,
+    minLength:2
+   },
+   lastName:{
+    type:String,
+    required:true
+   },
+   emailID:{
+    type:String,
+    required:true,
+    unique:true,
+    lowercase:true,
+    trim:true,
+    validate(value){
+      if(!validator.isEmail(value)){
+         throw new Error("Invalid Email")
+      }
+    }
+   },
+   password:{
+    type:String,
+    required:true,
+    validate(value){
+      if(!validator.isStrongPassword(value)){
+         throw new Error("Not a strong password")
+      }
+    }
+   },
+  
+},{timestamps:true})
+
+ userSchema.methods.getJwt = async function(){
+   const user = this;
+   const jwtToken = await jwt.sign({_id:user._id},"Dev@Tinder69",{expiresIn:"7d"});
+   return jwtToken;
+ }
+
+ userSchema.methods.comparePassword = async function(passwordInputByUser){
+   const user = this;
+   const passwordHash = user.password;
+   const isPasswordValid = await bcrypt.compare(passwordInputByUser,passwordHash);
+
+   return isPasswordValid;
+ }
+
+const User = mongoose.model("User",userSchema);
+
+module.exports=User;
