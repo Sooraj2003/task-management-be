19e9cf0341e5fac1716c3daf8800f2d4d435cf1d
Sooraj2003 <96384011+Sooraj2003@users.noreply.github.com>
2025-03-23 16:31:28 +0530

    created api's to add,get,update and delete a task

--- src/routes/task.js
+++ src/routes/task.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const Task = require("../models/task")
-const validateTask = require("../utils/validateTask")
+const {validateTask} = require("../utils/validation")
 
 
 // Creating a router for tasks
@@ -35,6 +35,101 @@ taskRouter.post("/task/create", async (req,res)=>{
 
     }
 })
+
+//Get the tasks
+taskRouter.get("/tasks",async (req,res)=>{
+    try{
+     const tasks = await Task.find();
+     //Checking if tasks is defined
+     if(!tasks){
+        throw new Error("Tasks is not found - db find error")
+     }
+     //Checking if tasks is empty
+     if(tasks.length==0){
+        throw new Error("Tasks is empty");
+     }
+
+     res.json({
+        message:"Tasks data",
+        tasks:tasks
+     })
+    }catch(err){
+      res.status(400).json({
+        errorMessage:err.message
+      })
+    }
+})
+
+//Update the particular task
+taskRouter.patch("/task/update/:id", async (req, res) => {
+    try {
+        //Validate the body
+        validateTask(req);
+        const { id } = req.params;
+        const updates = req.body;  // Contains the fields to be updated
+
+        const updatedTask = await Task.findByIdAndUpdate(id, updates, { new: true });
+
+        if (!updatedTask) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        res.json({
+            message: "Task updated successfully",
+            task: updatedTask
+        });
+
+    } catch (err) {
+        res.status(400).json({ errorMessage: err.message });
+    }
+});
+
+//Deleta a task
+taskRouter.delete("/task/delete/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const deletedTask = await Task.findByIdAndDelete(id);
+
+        if (!deletedTask) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        res.json({
+            message: "Task deleted successfully",
+            task: deletedTask
+        });
+
+    } catch (err) {
+        res.status(400).json({ errorMessage: err.message });
+    }
+});
+
+//Mark as complete - a particular task
+taskRouter.patch("/task/complete/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const updatedTask = await Task.findByIdAndUpdate(
+            id,
+            { status: "completed" },
+            { new: true } // Returns the updated document
+        );
+
+        if (!updatedTask) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        res.json({
+            message: "Task marked as completed",
+            task: updatedTask
+        });
+
+    } catch (err) {
+        res.status(400).json({ errorMessage: err.message });
+    }
+});
+
 
 
 module.exports = taskRouter
---
+++ src/utils/validation.js
@@ -0,0 +1,32 @@
+const validator = require('validator');
+
+const validateSignUp = (req)=>{
+    const {firstName,lastName,emailID} = req.body;
+
+    if(!firstName || !lastName){
+        throw new Error("Enter full name");
+    }else if(firstName>=4 && firstName<=50){
+        throw new Error("correct the length of name");
+    }else if(!validator.isEmail(emailID.trim())){
+        throw new Error("Invalid email")
+    }
+}
+const validateTask = (req)=>{
+
+ const {dueDate,priority,status} = req.body;
+    const format = 'YYYY-MM-DD';
+    if(!validator.isDate(dueDate,format)){
+        throw new Error("Invalid date type")
+    }
+
+    if(!["low","medium","high"].includes(priority.toLowerCase())){
+        throw new Error("Invalid priority type")
+    }
+    if(!["pending","completed"].includes(status.toLowerCase())){
+        throw new Error("Invalid status type")
+    }
+}
+module.exports = {
+    validateSignUp,
+    validateTask,
+}
